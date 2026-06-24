dba8c07c04b6aa18fa34fd53da47d4ae76231489
harry3905 <coolfire.prince0@gmail.com>
2026-06-24 18:58:26 +0530

    feat(editor): implement custom table block embed blot and clipboard matcher to allow nested HTML tables inside editor

--- client/src/components/RichTextEditor.jsx
+++ client/src/components/RichTextEditor.jsx
@@ -1,6 +1,31 @@
 import ReactQuill from 'react-quill';
 import 'react-quill/dist/quill.snow.css';
 
+const Quill = ReactQuill.Quill;
+
+if (Quill) {
+  const BlockEmbed = Quill.import('blots/block/embed');
+
+  class TableEmbedBlot extends BlockEmbed {
+    static create(value) {
+      const node = super.create();
+      node.setAttribute('contenteditable', 'false');
+      node.style.margin = '16px 0';
+      node.style.display = 'block';
+      node.innerHTML = value;
+      return node;
+    }
+
+    static value(node) {
+      return node.innerHTML;
+    }
+  }
+  TableEmbedBlot.blotName = 'table-embed';
+  TableEmbedBlot.tagName = 'div';
+  TableEmbedBlot.className = 'quill-table-embed';
+  Quill.register(TableEmbedBlot);
+}
+
 export default function RichTextEditor({ value, onChange }) {
   const modules = {
     toolbar: [
@@ -10,7 +35,15 @@ export default function RichTextEditor({ value, onChange }) {
       ['blockquote', 'code-block'],
       ['link', 'image'],
       ['clean']
-    ]
+    ],
+    clipboard: {
+      matchers: [
+        ['table', (node) => {
+          const Delta = Quill.import('delta');
+          return new Delta().insert({ 'table-embed': node.outerHTML });
+        }]
+      ]
+    }
   };
 
   const formats = [
@@ -19,7 +52,7 @@ export default function RichTextEditor({ value, onChange }) {
     'list', 'bullet',
     'blockquote', 'code-block',
     'link', 'image',
-    'table'
+    'table-embed'
   ];
 
   return (
